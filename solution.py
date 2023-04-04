fdff7f9d1c2fa8a107bd73b22db12cb225558754
D-Semenov <129876525+D-Semenov@users.noreply.github.com>
2023-04-04 21:58:03 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,6 +10,8 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p 
-    return (np.max(x)+np.min(x))/2,\
-           ((np.max(x)+np.min(x))/2)/(alpha**(1/len(x)))
+    #alpha = 1 - p
+    a1 = (1 - p)/2
+    a2 = (1 + p)/2
+    return (x.max() - 0.023)/(a2**(1/len(x))) + 0.023,\
+           (x.max() - 0.023)/(a1**(len(x))) + 0.023
